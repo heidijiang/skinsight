2c0d05a3ad0548f6cca676099d02bab5a9bfd9d4
Heidi Jiang <jiang.heidi@gmail.com>
2019-08-04 03:03:09 -0400

    update bert ckpt location

--- bert/start_bert.sh
+++ bert/start_bert.sh
@@ -1,7 +1,7 @@
 for col in acne texture dark_spot sensitive wrinkle redness
 do
 	export BERT_BASE_DIR=~/Documents/insight/skinsight/bert/bert_dev
-
+	ckpt=$(ls -t "model.ckpt"*".meta" | head -n 1)
 	for type in test #train dev 
 	do 
 		python run_classifier.py \
@@ -12,7 +12,7 @@ do
 		--data_dir=./results/$col/data_$type \
 		--vocab_file=$BERT_BASE_DIR/vocab.txt \
 		--bert_config_file=$BERT_BASE_DIR/bert_config.json \
-		--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+		--init_checkpoint=./results/$col/bert_output/bert_model${ckpt:11:2}.ckpt \
 		--max_seq_length=128 \
 		--train_batch_size=32 \
 		--learning_rate=2e-5 \
